eb41b07e79bbb8baae9fa975ef80e10f759ee587
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-01-12 00:38:31 +0700

    count comment still not get page access token

--- src/app/modules/inbox/inboxCtrl.js
+++ src/app/modules/inbox/inboxCtrl.js
@@ -5,8 +5,10 @@
 
 angular
     .module('dashboard')
-    .controller('inboxCtrl', function ($scope, $rootScope, pageService) {
+    .controller('inboxCtrl', function ($scope, $rootScope, pageService,$stateParams ) {
         console.log('inside dashboard controller');
+        $scope.pageId = $stateParams.pageId;
+        console.log($scope.pageId);
         $scope.getPost = function(){
             var page_token ="EAADKzAi0HZBEBALIMZBkOy3Xe4UEAtFLVZBknlZB1OXkcmIIvGSZCfuMucF1oN6OxjOYoEOFyK2FuebKV384hCw7OuYc7iwv5Vw4lgWD4iETHsJ9mfqIBal3HwGPBZAZAvJBX5DvsLprRuhBhsa3TH8xkzy2DR3WLUZD";
             FB.api (
@@ -15,12 +17,19 @@ angular
                     if (response && !response.error) {
                         console.log(response.feed.data);
                         var count = 0;
-                        var count_comment =0;
-                        console.log(response.feed.data.length);
-                        for(var i = 0; i<response.feed.data.length;i++ ){
-                           
+                        var feed = response.feed.data;
+                        for(var i = 0; i< feed.length;i++ ){
+                            if (feed[i].comments){
+                                var count_comment =0;
+                                console.log(feed[i].comments.data);
+                                for (var j= 0;j<feed[i].comments.data.length;j++) {
+                                    count_comment += feed[i].comments.data[j].comment_count;
+                                }
+                                console.log(count_comment);
+                                count += count_comment ;
+                            }
                         }
-
+                        console.log(count + feed.length);
                     }
                 }
             );
