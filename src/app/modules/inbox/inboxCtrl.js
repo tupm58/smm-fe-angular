1cdf0e2fe68d1ee59ca2eb1b714ce40d7e0d2a3f
Lannt <nguyenthilanuet96@gmail.com>
2017-01-13 11:43:25 +0700

    update view

--- src/app/modules/inbox/inboxCtrl.js
+++ src/app/modules/inbox/inboxCtrl.js
@@ -5,34 +5,35 @@
 
 angular
     .module('dashboard')
-    .controller('inboxCtrl', function ($scope, $rootScope, pageService,$stateParams ) {
+    .controller('inboxCtrl', function ($scope, $rootScope, pageService, $stateParams) {
         console.log('inside dashboard controller');
         $scope.pageId = $stateParams.pageId;
         console.log($scope.pageId);
-        $scope.getPost = function(){
-            var page_token ="EAADKzAi0HZBEBALIMZBkOy3Xe4UEAtFLVZBknlZB1OXkcmIIvGSZCfuMucF1oN6OxjOYoEOFyK2FuebKV384hCw7OuYc7iwv5Vw4lgWD4iETHsJ9mfqIBal3HwGPBZAZAvJBX5DvsLprRuhBhsa3TH8xkzy2DR3WLUZD";
-            FB.api (
-                "me?fields=feed{comments{comment_count},message}&access_token=" + page_token,
+        $scope.getPost = function () {
+            var page_token = "EAADKzAi0HZBEBALIMZBkOy3Xe4UEAtFLVZBknlZB1OXkcmIIvGSZCfuMucF1oN6OxjOYoEOFyK2FuebKV384hCw7OuYc7iwv5Vw4lgWD4iETHsJ9mfqIBal3HwGPBZAZAvJBX5DvsLprRuhBhsa3TH8xkzy2DR3WLUZD";
+            FB.api(
+                "me?fields=feed{comments{comment_count},message, id}&access_token=" + page_token,
                 function (response) {
                     if (response && !response.error) {
                         console.log(response.feed.data);
                         var feed = response.feed.data;
-                        for(var i = 0; i< feed.length;i++ ){
+                        for (var i = 0; i < feed.length; i++) {
                             var count = 0;
-                            if (feed[i].comments){
-                                var count_comment =0;
-                                for (var j= 0;j<feed[i].comments.data.length;j++) {
+                            if (feed[i].comments) {
+                                var count_comment = 0;
+                                for (var j = 0; j < feed[i].comments.data.length; j++) {
                                     count_comment += feed[i].comments.data[j].comment_count;
                                 }
-                                count = count_comment+feed[i].comments.data.length;
-                                console.log("post_id"+i+": " + count);
-                            }else{
+                                count = count_comment + feed[i].comments.data.length;
+                                console.log("post_id" + i + ": " + count);
+                            } else {
                                 count = count + 1;
-                                console.log("post_id"+i+": "+ count);
+                                console.log("post_id" + i + ": " + count);
                             }
                         }
                     }
                 }
             );
         }
+
     });
